e5a88dfdfc151771bf98150e59e7bd427a491ba5
jolantern <j.o.lantern0422@gmail.com>
2021-02-24 14:27:38 +0900

    順番を変

--- mitamae/default.rb
+++ mitamae/default.rb
@@ -2,10 +2,10 @@ package 'unzip'
 
 ENV["USER"] = ENV["HOME"].split("/").last
 
+include_recipe "asdf/default.rb"
 include_recipe "fish/default.rb"
 include_recipe "fish-theme/default.rb"
 
 include_recipe "bat/default"
-include_recipe "asdf/default.rb"
 include_recipe "peco/default.rb"
 include_recipe "ghq/default.rb"
