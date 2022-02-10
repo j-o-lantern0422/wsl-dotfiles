71bb77be331f8b433fc7a50a76379d41ee2d7702
kaji <nishihata@pepabo.com>
2022-02-10 15:42:11 +0900

    group

--- mitamae/asdf/default.rb
+++ mitamae/asdf/default.rb
@@ -6,12 +6,12 @@ end
 directory "#{ENV['HOME']}/.config/fish/completions" do
   action :create
   owner ENV["USER"]
-  group ENV["USER"]
+  group ENV["GROUP"]
 end
 
 remote_file "#{ENV['HOME']}/.config/fish/completions/asdf.fish" do
   source "#{ENV['HOME']}/.asdf/completions/asdf.fish"
   mode "644"
   owner ENV["USER"]
-  group ENV["USER"]
+  group ENV["GROUP"]
 end
