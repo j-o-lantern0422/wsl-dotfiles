fef9da010db9fa32eb7e45ab8043842535980602
kaji <nishihata@pepabo.com>
2022-02-10 16:43:19 +0900

    thema

--- mitamae/fish/config/config.fish
+++ mitamae/fish/config/config.fish
@@ -5,7 +5,7 @@ end
 
 function fish_prompt
   if test (uname -s) = "Darwin"
-
+    ./conf.d/dracula.fish
   else
     powerline -error $status -jobs (jobs -p | wc -l) --shell bare --max-width 40 -hostname-only-if-ssh -priority root,cwd,host,ssh,perms,git-branch,git-status,hg,jobs,exit,cwd-path,user
   end
