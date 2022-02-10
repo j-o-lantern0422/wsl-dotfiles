4bab977329a3a295a4cd47f75e2b3d3c9fe8bb25
kaji <nishihata@pepabo.com>
2022-02-10 17:02:09 +0900

    dir

--- mitamae/alacritty/default.rb
+++ mitamae/alacritty/default.rb
@@ -1,4 +1,10 @@
 if `uname -a`.include?("Darwin")
+  directory "#{ENV['HOME']}/.config/alacritty" do
+    action :create
+    owner ENV["USER"]
+    group ENV["GROUP"]
+  end
+
   remote_file "#{ENV['HOME']}/.config/alacritty/alacritty.yml" do
     source "./config/mac/alacritty.yml"
     owner ENV["USER"]
